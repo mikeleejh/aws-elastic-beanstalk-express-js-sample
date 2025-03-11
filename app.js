a99fb48dfa95c6964d7b3fc5cd59c43f53cfb14b
mikeleejh <mikelee.jh@gmail.com>
2025-03-11 17:55:31 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('HELLO 20250311'));
+app.get('/', (req, res) => res.send('HELLO 20250311 TEST TEST TEST TEST'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
